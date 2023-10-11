5e7893d912b6e96f754b5119c581cf5551b6e8b0
GylbarQ <r.vladik98@gmail.com>
2023-10-11 15:14:13 +0300

    Example5

---
+++ Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "admin")
+{
+    Console.WriteLine("Hi, my dear friend");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
